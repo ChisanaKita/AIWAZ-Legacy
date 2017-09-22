65ce1fa665a8fa4ca0357ade1944a66c1964beef
XiaoBeiLab <plkcwcs1a24@gmail.com>
2017-09-23 07:50:54 +0800

    Update skill.js

--- skill.js
+++ skill.js
@@ -125,16 +125,16 @@ var r1 = guild.roles.find("name", "科學");
 			member.addRole(r1).then(member.addRole(r2)).then(member.addRole(r3));
 
 			guild.channels.find('type', 'text').send(`🔊  Welcome \*\*${member.user.username}\*\* ( ${r2.name} ) Joined Our Server.\n🔸  Your Skill is : \`${r3.name}\` ${r3}
-			\*\*\_\_                                                                        ⚠️Warning⚠️                                                                        \_\_\*\*
+			\*\*\_\_                                                                              ⚠️Warning⚠️                                                                              \_\_\*\*
 			Please Follow The \*\*NAMO Protocol (Revised) Regulations\*\* Or You Will Get Kick By <@347782166811377667>\n
-			\n                                                         🔸🔹🔸\*\*\_\_   (NAMO Protocol [1.5])   \_\_\*\*🔸🔹🔸\n                                                     Use Command : /register To Register Your Identity`);
+			\n                                                  🔸🔹🔸\*\*\_\_   (NAMO Protocol [1.5])   \_\_\*\*🔸🔹🔸\n                                                 Use Command : /register To Register Your Identity`);
 		} else {
 			member.addRole(r1).then(member.addRole(r2));
 
 			guild.channels.find('type', 'text').send(`🔊  Welcome \*\*${member.user.username}\*\* ( ${r2.name} ) Joined Our Server.\n🔸  Your Skill is : \`NULL\`
-			\*\*\_\_                                                                        ⚠️Warning⚠️                                                                        \_\_\*\*
+			\*\*\_\_                                                                              ⚠️Warning⚠️                                                                              \_\_\*\*
 			Please Follow The \*\*NAMO Protocol (Revised) Regulations\*\* Or You Will Get Kick By <@347782166811377667>\n
-			\n                                                         🔸🔹🔸\*\*\_\_   (NAMO Protocol [1.5])   \_\_\*\*🔸🔹🔸\n                                                     Use Command : /register To Register Your Identity`);
+			\n                                                  🔸🔹🔸\*\*\_\_   (NAMO Protocol [1.5])   \_\_\*\*🔸🔹🔸\n                                                 Use Command : /register To Register Your Identity`);
 		}
 //-------------------------------------------------------------------send message to MISAKA-NETWORK
 	ser.channels.get('347618456335548427').send({embed: {
