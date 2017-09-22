8d9982ec2c200e52915b2391939787fd85dd8936
XiaoBeiLab <plkcwcs1a24@gmail.com>
2017-09-23 04:13:14 +0800

    Adding Base Files

---
+++ bot.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const version = require('./package.json').version;
+require('./util/eventLoader')(client);
+
+//call the skill.js
+client.on("guildMemberAdd", (member) => {
+	var getskill = require('./skill.js');
+	getskill.skill(client, member);
+});
+
+//reload module
+var reload = (message, cmd) => {
+	try {
+		delete require.cache[require.resolve('./commands/' + cmd)];
+		let cmdFile = require('./commands/' + cmd);
+	} catch (error) {
+		message.channel.send(`Problem loading \`${cmd}\` : ${error.message}`).then(response => response.delete(5000));
+	} finally {
+		message.channel.send('- Reload End -').then(response => response.delete(1000));
+	}
+};
+exports.reload = reload;
+
+
+client.login("MzQ3NzgyMTY2ODExMzc3NjY3.DHdZBA.UthInyT9rN38EuwCnngenJpmYz4");
