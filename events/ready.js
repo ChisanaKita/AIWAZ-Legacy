7a152aed51b3d2c21577a188bca2cd56b23917e6
XiaoBeiLab <plkcwcs1a24@gmail.com>
2017-09-25 20:02:57 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,15 +1,13 @@
 const version = require('../package.json').version;
 const fs = require('fs');
-let d = new Date();
-var HK_Offset = 8 * 60;
-d.setMinutes(d.getMinutes() + HK_Offset);
+let d = new Date().toLocaleString();
 
 module.exports = client => {
   let ser = client.guilds.find("name", "個人的現實");
 
   console.log('\n I\`m Online \n AIwaz 御坂網絡 : ' + version);
   client.user.setPresence( { game: {name: 'AIW' + ' 御坂網絡 ' + version, type: 0 }});
-  ser.channels.find('name', 'aiw-log').send(`Client Online at : ${d.toLocaleString()}`);
+  ser.channels.find('name', 'aiw-log').send(`Client Online at : ${d}`);
 
   fs.readdir('./commands/', (error, files) => {
     if (error) console.error(error);
