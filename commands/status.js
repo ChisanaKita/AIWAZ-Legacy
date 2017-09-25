2015a34cc2ee730a688a156242061877abba3ec0
XiaoBeiLab <plkcwcs1a24@gmail.com>
2017-09-25 20:27:14 +0800

    Delete status.js

--- commands/status.js
+++
@@ -1,6 +0,0 @@
-const Discord = require('discord.js');
-const version = require('../package.json').version;
-exports.run = (client, message, args) => {
-  message.delete();
-  client.user.setStatus(args.join(' '));
-}
