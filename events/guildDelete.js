77795c119e210be7b314f4b8cf54fb47ed1c1437
XiaoBeiLab <plkcwcs1a24@gmail.com>
2017-09-26 17:39:49 +0800

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,5 +1,12 @@
+const moment = require('moment-timezone');
+const d = new Date();
 module.exports = guild =>{
   let g = guild.name;
   let d = new Date();
-  console.log(`I have left =${g}=  at : ${d.toLocaleString()}`);
+  console.log(`I have left =${g}=  at : ${toTimeZone(d, "Asia/Hong_Kong")}`);
+  
+ function toTimeZone(d, zone) {
+  var format = 'YYYY/MM/DD HH:mm:ss';
+  return moment(d, format).tz(zone).format(format);
+ };
 };
