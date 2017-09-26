8805c60d90400f4ae7f510ace7c6f4336181e093
XiaoBeiLab <plkcwcs1a24@gmail.com>
2017-09-26 17:25:35 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,16 +1,19 @@
 const version = require('../package.json').version;
 const fs = require('fs');
-let d = new Date();
-var HK_Offset = 8 * 60;
-d.setMinutes(d.getMinutes() + HK_Offset);
-
+const d = new Date();
+const moment = require('moment-timezone');
 module.exports = client => {
-  let ser = client.guilds.find("name", "個人的現實");
+  var ser = client.guilds.find("name", "個人的現實");
 
-  console.log('\n I\`m Online \n AIwaz 御坂網絡 : ' + version);
+  console.log('I\`m Online at : ${toTimeZone(d, "Asia/Hong_Kong")} \n AIwaz 御坂網絡 : ' + version);
   client.user.setPresence( { game: {name: 'AIW' + ' 御坂網絡 ' + version, type: 0 }});
-  ser.channels.find('name', 'aiw-log').send(`Client Online at : ${d.toLocaleString()}`);
+  ser.channels.find('name', 'aiw-log').send(`Client Online at : ${toTimeZone(d, "Asia/Hong_Kong")}`);
 
+  function toTimeZone(d, zone) {
+   var format = 'YYYY/MM/DD HH:mm:ss';
+   return moment(d, format).tz(zone).format(format);
+  };
+  
   fs.readdir('./commands/', (error, files) => {
     if (error) console.error(error);
 
