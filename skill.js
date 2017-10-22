28c318c8885843dbbd55f17c336307f0077e7b0b
霞の水平線に#6118 <plkcwcs1a24@gmail.com>
2017-10-22 22:30:19 +0800

    Fix Base Function
    Change : Level Roles Name

--- skill.js
+++ skill.js
@@ -34,11 +34,11 @@ var r1 = guild.roles.find("name", "科學");
 	var SRandomInt = GetRandomInt(1, 5);
 //------------------------------------------------------------------------------------r2 role array
 	var ArrayLevel = [
-		"Level 0 - 無能力者",
-		"Level 1 - 低能力者",
-		"Level 2 - 異能力者",
-		"Level 3 - 強能力者",
-		"Level 4 - 大能力者"
+		"無能力者 - Level 0",
+		"低能力者 - Level 1",
+		"異能力者 - Level 2",
+		"強能力者 - Level 3",
+		"大能力者 - Level 4"
 	];
 //------------------------------------------------------------------------------------r2 Logic Gate
 	switch(SRandomInt)
