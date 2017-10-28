61491541aeed2d2260c76dd5ffda53847202143b
霞の水平線に#6118 <plkcwcs1a24@gmail.com>
2017-10-28 13:47:59 +0800

    Update message.js

--- events/message.js
+++ events/message.js
@@ -8,7 +8,7 @@ module.exports = message => {
 	const client = message.client;
 	let args = message.content.split(' ');
 	const command = args.shift().slice(prefix.length);
-	const guild = client.guilds.find("name", "個人的現實");
+	const guild = client.guilds.get("179445290795794432");
 	
 if (!command) {
 	return;
@@ -22,16 +22,16 @@ function toTimeZone(d, zone) {
 console.log(`
     >Message Event<
 Command (${command}) Trigger. At: ${toTimeZone(d, "Asia/Hong_Kong")}
-By: ${message.author.username}, From: ${message.guild.name}`);
+By: ${message.author.username}#${message.author.tag}, From: ${message.guild.name}`);
 
 	try {
-		guild.channels.find('name', 'aiw-log').send(`Command : ( ${command} ) Trigger \n${toTimeZone(d, "Asia/Hong_Kong")},\n${message.author.username}`);
+		guild.channels.find('name', 'aiw-log').send(`Command : ( ${command} ) Trigger \n${toTimeZone(d, "Asia/Hong_Kong")},\nBy: ${message.author.username}#${message.author.tag}`);
 		let cmdFile = require(`../commands/${command}`);
 		cmdFile.run(client, message, args);
 	} catch (error) {
 			console.log(`\n${error}`);
 			message.channel.send('⚠️Error : ' + error.message).then(response => response.delete(10000));
-			 client.channels.get('347618456335548427').send({embed: {
+			 guild.channels.find('name', 'aiw-log').send({embed: {
 			 	color: 10158080,
 			 	description: '\`' + '------ 御坂網絡  ' + version + ' ------' + '\`\n' + 'Failed Execute Command : ' + command + `\nInput By : ` + message.author.username + '\nAt : ' + toTimeZone(d, "Asia/Hong_Kong") + '\nFrom : ' + message.guild  + '\n\`\`\`js\n' + error.stack + '\`\`\`'
 			 }});
