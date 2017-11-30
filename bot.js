d008b2000187a23dccb5e089a180bb33e013e561
霞の水平線に#6118 <plkcwcs1a24@gmail.com>
2017-11-30 22:21:24 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,11 +4,9 @@ const version = require('./package.json').version;
 require('./util/eventLoader')(client);
 const Token = process.env.BOT_TOKEN;
 const fs = require('fs');
-
-fs.writeFile('./token.txt', Token, (err) => {
-  if (err) throw err;
-  console.log('The file has been saved!');
-});
+const moment = require('moment-timezone');
+const d = new Date();
+const timeout = ms => new Promise(res => setTimeout(res, ms));
 
 //call the skill.js
 client.on("guildMemberAdd", (member) => {
@@ -29,5 +27,36 @@ var reload = (message, cmd) => {
 };
 exports.reload = reload;
 
+//restart
+var restart = (client, message) => {
+ restart();
+async function restart() {
+  let response = await message.channel.send('\`S.Y.S.T.E.M. Shutting Down.....\`');
+  await response.delete(1000);
+  await client.destroy();
+  await timeout(3000);
+  await client.login(Token);
+  await client.user.setStatus('dnd');
+  await client.user.setPresence( { game: {name: '御坂網絡 ' + 'Connecting', type: 2 }});
+  let response1 = await message.channel.send('> \*\*[node.AI]\*\* watching: * -AIM- * \n> \*\*[node.AI]\*\* Starting AIwaz... \n> Loading.. \*\*MISAKA-NETWORK\*\* ' + '\`' + version + '\`')
+  await response1.delete(5000);
+  let response2 = await message.channel.send('> CORE Restart Complete..... \n> Awaiting \*\*readdir\*\* Callback...');
+  await response2.delete(3000);
+  await client.user.setPresence( { game: {name: '御坂網絡 ' + version, type: 3 }});
+  let response3 = await message.channel.send('> \*\*MISAKA-NETWORK\*\* Connected.... Callback Sueecss..');
+  await response3.delete(2000);
+  await client.user.setStatus('online');
+  let finreply = await message.channel.send({embed: {
+    color: 15597568,
+    description: `At : \`${toTimeZone(d, "Asia/Hong_Kong")}\` S.Y.S.T.E.M. Successfully Restarted..`
+  }});
+  await finreply.delete(5000);
+};
+exports.restart = restart;
+
+  function toTimeZone(d, zone) {
+    var format = 'YYYY/MM/DD HH:mm:ss';
+    return moment(d, format).tz(zone).format(format);
+  };
 
 client.login(Token);
