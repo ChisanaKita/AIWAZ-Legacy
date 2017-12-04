d8c0f034e6617afe4db243ef8d54fb9af9ec9775
霞の水平線に#6118 <plkcwcs1a24@gmail.com>
2017-12-04 11:28:11 +0800

    Update cal.js

--- commands/cal.js
+++ commands/cal.js
@@ -1,12 +1,19 @@
 exports.run = (client, message, args) => {
   message.delete();
-
+  
+  regex = new RegExp("^[0-9]");
   let question = args.join(' ');
 
-  try {
-    var answer = eval(question);
-    message.channel.send(`Question : ${args.join(' ')} =  \`${answer}\``);
-  } catch (error) {
-    message.channel.send(`⚠️Error : ${error.message}`);
+  if(question.test(regex)){
+    try {
+      var answer = eval(question);
+      message.channel.send(`Question : ${args.join(' ')} =  \`${answer}\``);
+    } catch (error) {
+      message.channel.send(`⚠️Error : ${error.message}`);
+    }
+  }else{
+    message.channel.send("Please Don't Input String, I Know You See Me Using Eval()", {code: 'js'});
   }
+  
+
 }
