f54a14f39a53a7983d7e381b89488f0d3c28c525
霞の水平線に#6118 <plkcwcs1a24@gmail.com>
2018-03-04 00:28:31 +0800

    Delete ready.js

--- events/ready.js
+++
@@ -1,7 +0,0 @@
-const version = require('../package.json').version;
-module.exports = client => {
-
-console.log('Online!');
-
-client.user.setPresence( {activity: {name: `御坂網絡 ${version}`, type: 3 }} );
-}
