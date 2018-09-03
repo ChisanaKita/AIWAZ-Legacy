b2eb8951e74b5a00aacac7d32ea589349f187f6c
霞の水平線に#6118 <plkcwcs1a24@gmail.com>
2018-09-04 07:29:47 +0800

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,21 +1,30 @@
-let prefix = "aiwaz.";
+const prefix = "aiwaz.";
 const Discord = require('discord.js');
 const version = require('../package.json').version;
 const fs = require('fs');
 
 module.exports = message => {
-	if (!message.content.startsWith(prefix)) {
-		let incoming_msg = message.content.toLowerCase().split(' ');
-		if (incoming_msg.length > 3) return;
-		if (incoming_msg.includes('good' && 'bot')) pet(message); return;
-	}
 	if (message.author.bot) return;
 
-	var n = new Date().toLocaleString();
 	const client = message.client;
+	const guild = client.guilds.find("id", "179445290795794432");		//logging server
+	var n = new Date().toLocaleString();
+
+	if (!message.content.startsWith(prefix)) {
+		let incoming_msg = message.content.toLowerCase().split(' ');
+		if (incoming_msg.length > 8) return;		//prevent mem overflow
+		if (incoming_msg.includes('good') && incoming_msg.includes('bot')) pet(message, guild, n); return;
+	}
+
 	let args = message.content.split(' ');		//to ary
 	const command = args.shift().slice(prefix.length).toLowerCase();
-	const guild = client.guilds.find("name", "FS113341A/3A");		//logging server
+
+	console.log(`
+__________________________${[n]}__________________________
+|User: ${message.author.username}
+|Try to execute command: "${command}" /w args: "${args}"
+|From: ${message.guild.name} #${message.channel.name}
+|Awaiting callback......`);
 
 	if (message.guild.iconURL()) {
 		var guild_icon = message.guild.iconURL();
@@ -25,24 +34,21 @@ module.exports = message => {
 
 	fs.open(`commands/${command}.js`, "r+", (error, fd) => {
 		if (error) {
-			if (error.code === 'ENOENT') {
-				console.log('Rejected.. (code: ENOENT)');
-				return;
-			}
+			if (error.code === 'ENOENT') console.log('|Rejected.. (code: ENOENT)\n|_______________________________________________________________________'); return;
 
 			let embed = new Discord.MessageEmbed()
-				.setAuthor(`_____________(AIwaz) α Lyrae DownLink ${version}_____________`, `${message.author.displayAvatarURL()}`)
+				.setAuthor(`__________(AIwaz) α Lyrae DownLink ${version}__________`, `${message.author.displayAvatarURL()}`)
 				.setColor("#FF0000")
 				.setTitle(`An unknown error was encountered.`)
-				.addField("Unknown Error: ", `line: 44 (File System)`)
+				.addField("Unknown Error: ", `line: 35:2 (fs.open() \`File System\`)`)
 				.addField("Debug:", `\`\`\`js\n${error}\`\`\``)
 				.setThumbnail(guild_icon)
 				.setFooter('Aiwaz ' + (version) + ' ©️ Copyright <2018> | <ChisanaKita> ','https://i.imgur.com/Fta2jMg.jpg')
 			guild.channels.find('name', 'aiw-log').send({embed});
-			console.log('Rejected.. (code: Unknown ERROR)');
+			console.log('|Rejected.. (code: Unknown ERROR)\n|_______________________________________________________________________');
 		}	else {
 			let embed = new Discord.MessageEmbed()
-				.setAuthor(`_____________(AIwaz) α Lyrae DownLink ${version}_____________`, `${message.author.displayAvatarURL()}`)
+				.setAuthor(`__________(AIwaz) α Lyrae DownLink ${version}__________`, `${message.author.displayAvatarURL()}`)
 				.setColor("#00FF88")
 				.setTitle(`Command: ( ${command} ) Trigger.`)
 				.addField("🔹 Time:", `${n}`, true)
@@ -52,13 +58,13 @@ module.exports = message => {
 				.setThumbnail(guild_icon)
 				.setFooter('Aiwaz ' + (version) + ' ©️ Copyright <2018> | <ChisanaKita> ','https://i.imgur.com/Fta2jMg.jpg')
 			guild.channels.find('name', 'aiw-log').send({embed});
-			console.log('Resolved.. (exit code: 0)');
 		}		//end log msg conditions
 
 		try {
 			let cmdFile = require(`../commands/${command}`);
-			cmdFile.run(client, message, args);
+			cmdFile.run(client, message, args)
 		} catch (error) {
+			is_error(error)
 			message.channel.send(`⚠️Error : ${error.message}`);
 			let embed = new Discord.MessageEmbed()
 				.setAuthor(`_____________(AIwaz) α Lyrae DownLink ${version}_____________`, `${message.author.displayAvatarURL()}`)
@@ -70,18 +76,18 @@ module.exports = message => {
 				.setThumbnail(guild_icon)
 				.setFooter('Aiwaz ' + (version) + ' ©️ Copyright <2018> | <ChisanaKita> ','https://i.imgur.com/Fta2jMg.jpg')
 			guild.channels.find('name', 'aiw-log').send({embed});
-			console.log(`Rejected.. (code: ${error.message})`);
+			console.log(`|Rejected.. \n${error.stack}\n|_______________________________________________________________________`);
+		} finally {
+			is_error('NULL');
 		}		//end try
 	});		//end fs
 
-	function pet(message) {
+	function pet(message, guild, n) {
 		message.react('💖');
-		message.channel.send(`
-			.░░░░░░░███████ ]▄▄▄▄▄▄▄▄
-		....▂▄▅█████████▅▄▃▂	 ☻/	(Tank You~ <3)
-		Il████████████████]    /▌
-		◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤	 /\\ `);
-		console.log('I\'ve been petted.');
-		
+		message.channel.send('Tank You~!');
+		guild.channels.find('name', 'aiw-log').send(`${n}: I\'ve been petted.`);
 	}
-}
+	function is_error(err) {
+		if (err == 'NULL') {console.log('|Resolved.. (exit code: 0)\n|_______________________________________________________________________')}
+	}
+}	//end of message event
