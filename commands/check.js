d6ddfbb86cbb873f62ae3f76e5f48f376c1e8b17
霞の水平線に#6118 <plkcwcs1a24@gmail.com>
2018-09-25 19:42:34 +0800

    Delete check.js

--- commands/check.js
+++
@@ -1,9 +0,0 @@
-const vn = require('../util/array.json')
-exports.run = (client, message, args) => {
-message.delete();
-
-  for (var i = 0; i < vn.VoiceChannel.length; i++) {
-    guild.channels.find('name', vn.VoiceChannel[i])
-  }
-
-}
